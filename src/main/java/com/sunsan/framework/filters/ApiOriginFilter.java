a679d2351672917bb09165f9d6cf2dad4b298881
yangzq@sunsan.com.cn <zqYang789>
2018-10-15 14:55:05 +0800

    架构调整

--- src/main/java/com/sunsan/framework/filters/ApiOriginFilter.java
+++ src/main/java/com/sunsan/framework/filters/ApiOriginFilter.java
@@ -6,8 +6,10 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.stereotype.Component;
 
 
@@ -26,14 +28,13 @@ public class ApiOriginFilter implements javax.servlet.Filter {
     public void doFilter(ServletRequest request, ServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
         HttpServletResponse res = (HttpServletResponse) response;
-        /*if (false) {
+        if (false) {
             String originHeader = ((HttpServletRequest) request).getHeader("Origin");
             if (ArrayUtils.contains(allowDomain, originHeader))
                 res.addHeader("Access-Control-Allow-Origin", originHeader);
         } else {
             res.addHeader("Access-Control-Allow-Origin", "*");
-        }*/
-        res.addHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+        }
         res.addHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PUT");
         res.addHeader("Access-Control-Allow-Headers", "Content-Type");
         res.addHeader("Access-Control-Max-Age", "3600");
